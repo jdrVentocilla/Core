571bf917ec73b06032b8c87e69bfb7d181439bcc
drventocilla <66335401+drventocilla@users.noreply.github.com>
2023-06-20 19:32:41 -0500

    feature : Primera carga

---
+++ InfraestructuraADO/V1/Repository/RepositoryBase.cs
@@ -0,0 +1,38 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Core.InfraestructuraADO.V1.Repository
+{
+
+
+    public abstract class RepositoryBase
+
+    {
+        /// <summary>
+        /// Esta propiedad de va eliminar la siguiente semana
+        /// </summary>
+        protected string selectQuery = "";
+        protected MySqlTransaction transaction;
+        protected MySqlConnection context;
+
+
+        protected MySqlCommand CreateCommand(string query)
+        {
+            MySqlCommand comandoSQL = new MySqlCommand(query, context, transaction);
+            comandoSQL.CommandTimeout = 900;
+
+            return comandoSQL;
+        }
+        protected MySqlCommand CreateCommand()
+        {
+            MySqlCommand comandoSQL = new MySqlCommand(context, transaction);
+            comandoSQL.CommandTimeout = 900;
+
+            return comandoSQL;
+        }
+
+    }
+}
